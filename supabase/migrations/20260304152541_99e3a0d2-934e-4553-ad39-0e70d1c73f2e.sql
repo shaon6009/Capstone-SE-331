42466040856d467fcb518e4222e8f79b98b485e0
MD Ismail Hossain <157038690+ismail-2901@users.noreply.github.com>
2026-03-05 02:33:35 +0600

    Delete supabase directory

--- supabase/migrations/20260304152541_99e3a0d2-934e-4553-ad39-0e70d1c73f2e.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE OR REPLACE FUNCTION public.generate_anon_name()
-RETURNS TEXT
-LANGUAGE sql
-STABLE
-SET search_path = public
-AS $$
-  SELECT 'Anon-' || substr(md5(random()::text), 1, 5);
-$$;
